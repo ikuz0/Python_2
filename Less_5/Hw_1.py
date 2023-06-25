10805fefc112e52fd8a92e57300ec692fcd41a89
Ikuz0 <Kuznetsovilya469@yandex.ru>
2023-06-26 00:24:30 +0300

    Less_5

---
+++ Less_5/Hw_1.py
@@ -0,0 +1,10 @@
+def split_path(abs_path: str) -> tuple():
+    list_abs_path = abs_path.split('\\')
+    list_last_elem = list_abs_path[-1].split('.')
+    path = '\\'.join(list_abs_path[0:-1])
+    name = list_last_elem[0]
+    expansion = list_last_elem[1]
+    return path, name, expansion
+
+abs_path = r'C:\Дата\Предмет\КИМ.txt'
+print(split_path(abs_path))
