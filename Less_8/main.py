ebd8c910db59804bd4876809cc90cc3f47dc2771
Ikuz0 <Kuznetsovilya469@yandex.ru>
2023-07-09 23:07:11 +0300

    Less_9

--- Less_8/main.py
+++ Less_8/main.py
@@ -1,11 +1,6 @@
-#from create_json import create_json
 from pathlib import Path
-#from add_name_to_json import ui, read_json
 from save_file_info import get_dirs_json, write_json, write_csv, write_pickle
 if __name__ == '__main__':
-    # create_json(Path('../work_with_files_pack/result.txt'))
-    # ui()
-    # print(type(read_json()))
     dict_json = get_dirs_json('C:\\Users\\Kuzne\\Desktop\\Python_new_lesson\\Less_2\\less_8')
     write_json('dir_info_json.json', dict_json)
     write_csv('dir_info_csv.csv', dict_json)
